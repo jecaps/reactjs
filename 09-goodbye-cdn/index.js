af550a015a6bf37580dddf8439d8590b028c1eec
Jerika Kanz <jerikacapila@gmail.com>
2022-04-11 19:49:15 +0200

    setting up

---
+++ 09-goodbye-cdn/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+var navbar = React.createElement(
+  "nav",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Bob's Bistro"
+  ),
+  React.createElement(
+    "ul",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Menu"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "About"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Contact"
+    )
+  )
+);
+
+ReactDOM.render(navbar, document.getElementById("root"));
