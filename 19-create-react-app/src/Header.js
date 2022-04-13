f85fc014385eebafb9cf50fe9fffa22ae15398b7
Jerika Kanz <jerikacapila@gmail.com>
2022-04-13 10:32:47 +0200

    Set up a local React environment with Create React App

---
+++ 19-create-react-app/src/Header.js
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+          className="nav-logo"
+          alt="react-logo"
+        />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
