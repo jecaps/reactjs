5d47306395db824ed057252660bec55bee3cdcaf
Jerika Kanz <jerikacapila@gmail.com>
2022-04-14 16:12:37 +0200

    Add Background Logo

---
+++ 27-add-background-logo/src/index.js
@@ -0,0 +1,18 @@
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
