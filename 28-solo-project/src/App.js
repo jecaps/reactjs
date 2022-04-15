884da0f0b98c3288f786befe2d60dc9ef4c29ed6
Jerika Kanz <jerikacapila@gmail.com>
2022-04-15 10:41:43 +0200

    About

--- 28-solo-project/src/App.js
+++ 28-solo-project/src/App.js
@@ -1,9 +1,11 @@
 import Info from "./components/Info";
+import About from "./components/About";
 
 export default function App() {
   return (
     <div className="card">
       <Info />
+      <About />
     </div>
   );
 }
---
+++ 28-solo-project/src/components/About.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div className="card--about">
+      <p className="card--about_title">About</p>
+      <p>
+        I am a frontend developer with a particular interest in making thins
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </div>
+  );
+}
