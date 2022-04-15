a6103f1ea5b25fff61e9ce6b2608d39a6115e643
Jerika Kanz <jerikacapila@gmail.com>
2022-04-15 10:59:27 +0200

    Interests

--- 28-solo-project/src/App.js
+++ 28-solo-project/src/App.js
@@ -1,11 +1,13 @@
 import Info from "./components/Info";
 import About from "./components/About";
+import Interest from "./components/Interest";
 
 export default function App() {
   return (
     <div className="card">
       <Info />
       <About />
+      <Interest />
     </div>
   );
 }
---
+++ 28-solo-project/src/components/Interest.js
@@ -0,0 +1,11 @@
+export default function Interest() {
+  return (
+    <div className="card--interest">
+      <p className="card--interest_title">Interests</p>
+      <p>
+        Food expert. Music scholar. Reader. Interenet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
